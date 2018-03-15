1f897b7f042179853f77e0f6190dcb5ca931718b
cauan_kms <cauan.kms@gmail.com>
2018-03-14 22:23:53 -0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/LPI_FunctionsTask.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LPI_FunctionsTask.dir/programa1.c.o"
+  "LPI_FunctionsTask.pdb"
+  "LPI_FunctionsTask"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LPI_FunctionsTask.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
