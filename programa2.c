1e03b8130b6ab61836db95082f40a5d9fa2cf40d
cauan_kms <cauan.kms@gmail.com>
2018-03-21 20:28:58 -0300

    Ok, I couldnt make it work, so I'm moving on since the other ones are working fine

--- programa2.c
+++ programa2.c
@@ -2,13 +2,60 @@
 #include <math.h>
 #include <ctype.h>
 
-void showEmployeeSalary(float salary){
-    //THIS GOTTA CHANGE!!!!
-    float realSal, taxes;
-    taxes = (salary / 100) * 20;
-    realSal = salary - taxes;
+float calculateTaxes(float salary){
+    float taxes = 0, taxValor[] = {0, 7.5, 15, 22.5, 27.5}, salaryCalc = salary;
+    float salaryClassify[] = {1903.98, 2826.65, 3751.05, 4664.68};
 
-    printf("\n%2.f\n", realSal);
+
+    int i;
+    for(i = 0; i < 4; i++){
+        //if(salaryCalc < salaryClassify[0]){break;}
+        if(salaryCalc < 0){break;}
+
+        if(salaryCalc > salaryClassify[i]){
+            //salaryCalc -= salaryClassify[i];
+            taxes += (salaryCalc / 100) * taxValor[i];
+
+
+            //salaryCalc -= salaryClassify[i];
+            //salaryCalc = salaryCalc - salaryClassify[i];
+        }
+
+        //salaryCalc = salaryCalc - salaryClassify[i];
+
+    }
+
+    /*
+
+    if(salary <= 1903.98){
+        taxes = 0;
+
+    }else if(salary >= 1903.99 && salary <= 2826.65){
+        taxes = 7.5;
+
+    }else if(salary >= 2826.66 && salary <= 3751.05){
+        taxes = 15;
+
+    }else if(salary >= 3751.06 && salary <= 4664.68){
+        taxes = 22.5;
+
+    }else{
+        taxes = 27.5;
+
+    }*/
+
+    return taxes;
+}
+
+void showEmployeeSalary(float salary){
+    float realSal, taxes;
+
+    //taxes = (salary / 100) * 20;
+    taxes = calculateTaxes(salary);
+
+    //realSal = salary - taxes;
+
+    printf("\n%f\n", taxes);
 
 }
 
@@ -47,7 +94,14 @@ void calculateNewSalary(float salary){
 
 }
 
-void classifySalary(){
+void classifySalary(float salary){
+    if(salary <= 7000){
+        printf("\nMal remunerado.\n");
+
+    }else{
+        printf("\nBem remunerado.\n");
+
+    }
 
 }
 
@@ -87,6 +141,10 @@ int main() {
                 break;
 
             case 'C':
+                printf("Qual o teu sal.?");
+                scanf("%f", &emSal);
+
+                classifySalary(emSal);
 
                 break;
 
